e1986dd00437f5b1a93e25c0b53c8e56f46ae6d0
alexcarne <al.alex.carnero.tapia@iesportada.org>
2024-10-31 09:13:46 +0100

    layout hecho

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
+let btn = document.querySelector("button");
 
+function inicializar() {
+  añadirstyle();
+  btn.addEventListener("click", function (event) {
+    event.preventDefault();
+  });
+}
+function añadirstyle() {
+  let btnclass = "btn-add";
+  let divclasses = [
+    "container",
+    "search",
+    "li-container",
+    "empty",
+    "task-count",
+  ];
+  let divs = document.querySelectorAll("div");
+  let counter = 0;
+  divs.forEach((div) => {
+    div.classList.add(divclasses[counter]);
+    counter++;
+  });
+  btn.classList.add(btnclass);
+}
+function añadirTask() {
+  let divtareas = document.getElementsByClassName("li-container");
+    let nomtarea = document.querySelector("input").textContent;
+    
+
+}
+
+document.addEventListener("DOMContentLoaded", inicializar);
